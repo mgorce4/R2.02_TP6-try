f2653e5e3ef9083e53ecb007d9ada04f2f2f7905
martin <martin.clerot@etu.unilim.fr>
2025-05-12 17:23:13 +0200

    add methods generateCardValue and loadCardImage in CardGameTools

--- src/unilim/info/ihm/tp6/exo3/tools/CardGameTools.java
+++ src/unilim/info/ihm/tp6/exo3/tools/CardGameTools.java
@@ -1,5 +1,20 @@
 package unilim.info.ihm.tp6.exo3.tools;
 
+import java.util.Random;
+
 public class CardGameTools {
+	
+	public static Integer generateCardValue() {
+        Random random = new Random();
+        return random.nextInt(12) + 1;
+	}
+	
+	public static void loadCardImage(Integer value) {
+		if (value > 0 && value < 13) {
+			System.out.println("la carte numero " + value + " à été chargé");
+		} else {
+			System.out.println("veuillez choisir un nombre entre 1 et 12");
+		}
+	}
 
 }
