1378b76df7da7fb9465b040ec24007d563d9243b
martin <martin.clerot@etu.unilim.fr>
2025-05-12 17:16:12 +0200

    rename images + add CardGameTools

--- src/unilim/info/ihm/tp6/exo3/model/Card.java
+++ src/unilim/info/ihm/tp6/exo3/model/Card.java
@@ -1,5 +1,19 @@
 package unilim.info.ihm.tp6.exo3.model;
 
 public class Card {
+	
+    private final Integer value;
+
+
+    public Card(Integer value) {
+        if (value == null || value < 1 || value > 12) {
+            throw new IllegalArgumentException("La valeur doit être comprise entre 1 (As) et 12 (Reine)");
+        }
+        this.value = value;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
 
 }
---
+++ src/unilim/info/ihm/tp6/exo3/tools/CardGameTools.java
@@ -0,0 +1,5 @@
+package unilim.info.ihm.tp6.exo3.tools;
+
+public class CardGameTools {
+
+}
